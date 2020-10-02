781b2e39d9a6ab03ebfb87101b21384fb2e067c8
mike <mikesapienzaaudio@gmail.com>
2020-10-01 20:20:26 -0500

    final product

--- bin/run.rb
+++ bin/run.rb
@@ -4,9 +4,9 @@ require_relative "../app/recipe_app.rb"
 $idiot_sandwich = "../lib/sounds/idiot_sandwich.mp3"
 require_relative "../lib/beet.rb"
 
-# $idiot_sandwich = Sound.new("lib/sounds/idiot_sandwich_clip.mp3")
+$idiot_sandwich = Sound.new("lib/sounds/idiot_sandwich_clip.mp3")
 
 app = RecipeApp.new
-# $idiot_sandwich.play
+$idiot_sandwich.play
 app.run 
 
